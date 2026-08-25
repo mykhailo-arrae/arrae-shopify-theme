de41d6d57acbe3e13acab48a386dc82d73b7f0e1
Autobot <engineering@vaangroup.com>
2026-08-25 08:58:07 +0000

    Backup other files - 156050751680@arrae-test
    Store: arrae-test
    Theme Name: Vaan UAT
    Theme ID: 156050751680

--- assets/standalone-build-demo.js
+++
@@ -1,2 +0,0 @@
-/*! DO NOT EDIT: This file is auto-generated and will be overwritten. [build-fingerprint:assets:source={/app/_js/standalone/standalone-build-demo/index.tsx}] */
-"use strict";(()=>{var run=async()=>{console.warn("This is a placeholder script")};run().catch(err=>{console.error(err)});})();
