cc092cfad46a2c4055501d091192d6ec1e4536b1
Autobot <engineering@vaangroup.com>
2026-08-26 03:17:31 +0000

    Backup other files - 156050751680@arrae-test
    Store: arrae-test
    Theme Name: Vaan UAT
    Theme ID: 156050751680

--- assets/tvg-section-value-propositions.js
+++
@@ -1,3 +0,0 @@
-"use strict";/*! DO NOT EDIT: This file is auto-generated and will be overwritten. [build-fingerprint:assets] */(self.webpackChunktvg_shopify_theme=self.webpackChunktvg_shopify_theme||[]).push([["9681"],{7196:function(__unused_webpack_module,__unused_webpack___webpack_exports__,__webpack_require__){
-(0,__webpack_require__(4339).initSection)(".js-value-propositions",()=>({unload:null}))}},function(__webpack_require__){__webpack_require__.O(0,["5247"],function(){return __webpack_require__(__webpack_require__.s=7196)}),__webpack_require__.
-O()}]);
